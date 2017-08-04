da2b886242150cb7a509eefc5cf6bcdc78459ac3
Martin Bee <martbee88@gmail.com>
2017-08-04 08:42:06 -0500

    feat: add user data, read file, add some routes

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 import express from 'express';
 import fs from 'fs';
+import { startCase } from 'lodash';
+
+const users = [];
+
+fs.readFile('./data/users.json', { encoding: 'utf8' }, (err, data) => {
+  if (err) throw err;
+
+  JSON.parse(data).forEach((user) => {
+    const { name } = user;
+
+    name.full = startCase(`${name.first} ${name.last}`);
+    users.push(user);
+  });
+});
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  const buffer = users.reduce((buffer, user) => (
+    buffer + `<a href="${user.username}">${user.name.full}<br/></a>`
+  ), '');
+
+  res.send(buffer);
+});
+
+app.get(/big.*./, (req, res, next) => {
+  console.log('BIG USER ACCESS');
+  next();
+});
+
+app.get('/:username', (req, res) => {
+  const username = req.params.username;
+
+  res.send(username);
 });
 
 app.listen(PORT, () => console.log(`Express is listening at ${PORT}`));
